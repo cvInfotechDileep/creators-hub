65f3e4f8f577feb604eedae7abb8a74aa6eada49
cvinfotechDileep <dileepmaurya@cvinfotech.com>
2024-08-03 15:14:32 +0530

    redirect feature

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // base: 'https://creators-hub-plum.vercel.app/',
   plugins: [react()],
+  server: {
+    historyApiFallback: true
+  }
   // resolve: {
   //   alias: [{ find:"@", replacement: "/src"}],
   // }
